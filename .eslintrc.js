6d77cd38dd4a21e44f01acd3d03bfb8fb5ad9a37
Mikita Kandratsyeu <40242552+mikita-kandratsyeu@users.noreply.github.com>
2023-03-05 13:56:04 +0300

    chore: setting up (#1)
    * chore: added starting file for project
    
    * chore: added lint progress
    
    * chore: github settings
    
    * chore: update yml
    
    * chore: update yml
    
    * chore: update yml
    
    * chore: update yml
    
    * chore: update yml
    
    * chore: update yml
    
    * chore: update yml

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', "file-progress"],
+  extends: [
+    "plugin:import/recommended",
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "file-progress/activate": 1,
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
